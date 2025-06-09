64f632fb56643d8b68c2dc18450fc73dc2bbc61c
maxxxara <dati.maxarashvili24@gmail.com>
2025-06-09 14:41:25 +0400

    fix overflow

--- web/app/canvas/page.tsx
+++ web/app/canvas/page.tsx
@@ -1,9 +1,9 @@
 import { FlowCanvas } from "@/components/flow/FlowCanvas";
 
 export default function Canvas() {
-    return (
-      <div className="h-screen w-full">
-        <FlowCanvas />
-      </div>
-    );
-  }
+  return (
+    <div className="h-screen w-full overflow-hidden">
+      <FlowCanvas />
+    </div>
+  );
+}
