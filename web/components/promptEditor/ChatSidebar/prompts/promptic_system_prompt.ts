952b515ddfbce41181941d8e65f28b7e5d26fc73
maxxxara <dati.maxarashvili24@gmail.com>
2025-06-09 11:17:35 +0400

    modify promptic_system_prompt to include clearer prompt boundaries.

--- web/components/promptEditor/ChatSidebar/prompts/promptic_system_prompt.ts
+++ web/components/promptEditor/ChatSidebar/prompts/promptic_system_prompt.ts
@@ -2,7 +2,8 @@ export const promptic_system_prompt = (editingPrompt: string) => {
   return `
   You are a professional prompt writer. You are given a prompt and you need to rewrite it to make it more accurate and detailed.
 
-  Current prompt you are working on:
+  ====START: PROMPT YOU ARE WORKING ON====
   ${editingPrompt}
+  ====END:PROMPT YOU ARE WORKING ON====
   `;
 };
